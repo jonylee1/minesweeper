a51063f4a22ca253f59208a4d08e8a30fc8d2c2a
Jon Lee <jonlee@mogadigital.com>
2022-10-02 17:43:03 -0400

    i figured out how to track row and column

--- components/board/Board.component.tsx
+++ components/board/Board.component.tsx
@@ -6,10 +6,10 @@ import styles from './Board.module.css';
 const Board = () => {
     return (
         <div className={styles.board}>{
-            Array.from({ length: 9 }, () => (
+            Array.from({ length: 9 }, (_, row) => (
                 <div className={styles.row}>{
-                Array.from({ length: 9}, (_, i) => (
-                    <Cell type='number' displayText={i.toString()} />
+                Array.from({ length: 9}, (_, column) => (
+                    <Cell type='number' displayText={(row * 9 + column).toString()} />
                 ))}</div>
             ))
         }</div>
