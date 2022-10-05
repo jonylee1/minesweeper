2dd6ec6aef77f11d8a36e75816a86afd45b27d78
Jon Lee <jonlee@mogadigital.com>
2022-10-05 18:29:48 -0400

    skibbity do wop

--- components/board/Board.component.tsx
+++ components/board/Board.component.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import Cell from "../cell/Cell.component";
 import styles from './Board.module.css';
 
+// TODO: Add a handler for first click to generate the board.
+// TODO: Console output the cells coordinates when we click on a cell.
+
+const firstClick = () => {
+    console.log('you clicked me');
+}
 
 const Board = () => {
     return (
-        <div className={styles.board}>{
+        <div className={styles.board} onClick={firstClick}>{
             Array.from({ length: 9 }, (_, row) => (
-                <div className={styles.row}>{
+                <div key={`${row}`} className={styles.row}>{
                 Array.from({ length: 9}, (_, column) => (
-                    <Cell key={`${row * 9 + column}`} type='number' displayText={(row * 9 + column).toString()} />
+                    <Cell 
+                        key={`${row * 9 + column}`}
+                        type='number'
+                        displayText={(row * 9 + column).toString()}
+                        x={row}
+                        y={column}
+                    />
                 ))}</div>
             ))
         }</div>
--- components/cell/Cell.component.tsx
+++ components/cell/Cell.component.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import styles from './Cell.module.css';
 
-const Cell = (props: any) => {
-    const [hidden, setHidden] = React.useState(true)
+enum CELL_TYPE {
+    NUMBER,
+    MINE
+}
 
-    const toggleHidden = (event: any) => {
-        setHidden(!hidden);
+type CellProps = {
+    type: string;
+    displayText: string;
+    x: number;
+    y: number;
+}
+
+const Cell: React.FunctionComponent<CellProps> = (props) => {
+    const [visible, setVisible] = React.useState(false)
+
+    const handleClick = (event: any) => {
+        setVisible(!visible);
         event.currentTarget.disabled = true;
+        event.currentTarget.classList.add(styles.clicked);
+    }
+
+    const handleRightClick = (event: any) => {
+        console.log('right-click captured');
+        event.preventDefault();
     }
 
     return (
-        <button className={styles.cell} onClick={toggleHidden}>{hidden ? '' : props.displayText}</button>
+        <button className={styles.cell} onClick={handleClick} onContextMenu={handleRightClick}>{visible ? props.displayText : ''}</button>
     )
 }
 
