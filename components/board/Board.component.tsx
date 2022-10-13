48ea1b634a609409aa429570e70736f18ce3a2f2
Jon Lee <jonlee@mogadigital.com>
2022-10-13 18:52:37 -0400

    We got the board to update a cell change. we can use this to increment the count of a cell during board generation.

--- components/board/Board.component.tsx
+++ components/board/Board.component.tsx
@@ -1,40 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Cell from "../cell/Cell.component";
 import styles from './Board.module.css';
 
-// TODO: Add a handler for first click to generate the board.
-//       Generate the board
-//       Execute the mouse click event on the cell that was clicked.
-// TODO: Console output the cells coordinates when we click on a cell.
 
-// const handleFirstClick = (event: any) => {
-//     // remove event listener
-//     // generate the board
-//     // allow cell to process click
-//     generateBoard(event);
-// }
 
 /**
  * Should be called after the user clicks a cell for the first time
  * @param event 
  */
 const generateBoard = (row: number, column: number) => {
-    // Take in the cell that was clicked so that we do not place a mine in that cell.
-    // Randomly place 10 mines
-    // Generate the numbers around the mines.
+
     console.log('generate board');
     
 }
 
 // maybe use a HashMap if the 2-dimensional array thing doesn't pan out
 let boardArray: string[][] = [
-                                ['x',    '2',    '1'],
-                                ['2',    'x',    '1'],
-                                ['1',    '1',    '1']
+                                ['x', '1', '0', '0', '0'],
+                                ['1', '2', '2', '2', '1'],
+                                ['0', '2', 'x', 'x', '2'],
+                                ['1', '2', '3', '3', 'x'],
+                                ['1', 'x', '1', '1', '1']
                             ];
 
+const BoardState = React.createContext(boardArray);
+
+
+
 const Board = () => {
-    let gameStarted = false;
+    const [board, setBoard] = useState(boardArray);
+    const [cell, setCell] = useState(boardArray[0][1]);
 
     /**
      * Should be called after every cell is clicked.
@@ -42,22 +37,31 @@ const Board = () => {
      * @param column 
      */
     const checkBoardState = (row: number, column: number) => {
-        console.log(`checkboardstate ${row} x ${column}`);
-        console.log(`checkboardstate/generated: ${gameStarted}`);
-        if (gameStarted) {
-            console.log(`checking cell ${row} x ${column}`);
-            // reveal the cell
-            // if (boardArray[row][column] === 'x') {
-            //     console.log('lose');
-            // } else {
-            //     console.log('win');
-            // }
-        } else {
-            gameStarted = true;
-            // generate board
-            generateBoard(row, column);
-            checkBoardState(row, column);
-        }
+        boardArray[0][1] = (parseInt(boardArray[0][1]) + 1).toString();
+        setBoard([...boardArray]);
+        //setCell((parseInt(boardArray[0][1]) + 1).toString());
+        
+        // if (boardArray[row][column] === 'x') {
+        //     // lost
+        // } else {
+            
+        // }
+    }
+
+    const initializeBoard = () => {
+        return Array.from({ length: 5 }, (_, row) => (
+            <div key={`${row}`} className={styles.row}>{
+            Array.from({ length: 5}, (_, column) => (
+                <Cell 
+                    handleClick={checkBoardState}
+                    key={`${row * 5 + column}`}
+                    type='number'
+                    displayText={board[row][column]}
+                    x={column}
+                    y={row}
+                />
+            ))}</div>
+        ))
     }
 
     useEffect(() => {
@@ -65,21 +69,7 @@ const Board = () => {
     })
 
     return (
-        <div className={styles.board}>{
-            Array.from({ length: 3 }, (_, row) => (
-                <div key={`${row}`} className={styles.row}>{
-                Array.from({ length: 3}, (_, column) => (
-                    <Cell 
-                        handleClick={checkBoardState}
-                        key={`${row * 3 + column}`}
-                        type='number'
-                        displayText={boardArray[row][column]}
-                        x={column}
-                        y={row}
-                    />
-                ))}</div>
-            ))
-        }</div>
+        <div className={styles.board}>{initializeBoard()}</div>
     );
 }
 
